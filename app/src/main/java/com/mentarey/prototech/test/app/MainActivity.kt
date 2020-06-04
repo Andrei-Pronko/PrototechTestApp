7f97ff16265cdb5cf512e764c128688c6d6ce2fc
Andrei Pranko <djmentarey@gmail.com>
2020-06-04 16:53:25 +0300

    updated main activity

--- app/src/main/java/com/mentarey/prototech/test/app/MainActivity.kt
+++ app/src/main/java/com/mentarey/prototech/test/app/MainActivity.kt
@@ -2,10 +2,18 @@ package com.mentarey.prototech.test.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mentarey.prototech.test.app.ext.replaceContainer
+import com.mentarey.prototech.test.app.ui.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        openLoginScreen()
+    }
+
+    private fun openLoginScreen() {
+        replaceContainer(R.id.fragment_container, LoginFragment())
     }
 }
