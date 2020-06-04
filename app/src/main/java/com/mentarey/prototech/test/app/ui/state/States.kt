e288b63bf54eb760b3bafc44d7616a4067d93d6d
Andrei Pranko <djmentarey@gmail.com>
2020-06-04 16:52:42 +0300

    added UI states

---
+++ app/src/main/java/com/mentarey/prototech/test/app/ui/state/States.kt
@@ -0,0 +1,13 @@
+package com.mentarey.prototech.test.app.ui.state
+
+import com.mentarey.prototech.test.app.domain.ForexSignalUI
+
+sealed class UserAuthState {
+    object Success : UserAuthState()
+    class Error(val message: String?) : UserAuthState()
+}
+
+sealed class RemoteSignal {
+    class Data(val forexSignalUIListUI: List<ForexSignalUI>) : RemoteSignal()
+    class Error(val message: String?) : RemoteSignal()
+}
